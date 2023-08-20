1c4cf0ad67353c9893156226dfaef956c9594475
Rashad2890 <132349192+Rashad2890@users.noreply.github.com>
2023-08-20 17:03:39 +0300

    Update main.h

--- main.h
+++ main.h
@@ -2,7 +2,6 @@
 #define FUNCTIONS
 
 extern char **get_command(void);
-extern int setenv(const char *name, const char *value, int overwrite);
-char *which(const char *command);
+extern char *which(const char *command);
 
 #endif /*FUNCTIONS*/
